53759f996b2bbd2e955064a84ab9cb2f2422fee6
davidcruzr92 <davidcruzr92@outlook.com>
2022-07-06 20:57:09 -0600

    Create DTO and ViewModel folder

---
+++ Models/DTOS/EmployeeDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWT.Models.DTOS
+{
+    public class EmployeeDTO
+    {
+        public string NationalIdnumber { get; set; } = null!;
+        public string? EmployeeName { get; set; }
+        public string LoginId { get; set; } = null!;
+        public string JobTitle { get; set; } = null!;
+        public DateTime BirthDate { get; set; }
+        public string MaritalStatus { get; set; } = null!;
+        public string Gender { get; set; } = null!;
+        public DateTime HireDate { get; set; }
+        public short VacationHours { get; set; }
+        public short SickLeaveHours { get; set; }
+    }
+}
---
+++ Models/ViewModels/EmployeeVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWT.Models.ViewModels
+{
+    public class EmployeeVM
+    {
+        public string? EmployeeName { get; set; }
+        public string LoginId { get; set; } = null!;
+        public string JobTitle { get; set; } = null!;
+        public DateTime BirthDate { get; set; }
+    }
+}
