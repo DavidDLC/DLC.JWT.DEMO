8bf07c6d7050c01288df11c993579e710e10bdd2
davidcruzr92 <davidcruzr92@outlook.com>
2022-07-06 21:50:44 -0600

    Add login endpoint + create variables on appsetting.json for JWT

--- Demo.API/Controllers/UserManagementController.cs
+++ Demo.API/Controllers/UserManagementController.cs
@@ -1,6 +1,9 @@
 ﻿using JWT.Business.Contracts;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace JWT.API.Controllers
 {
@@ -9,16 +12,46 @@ namespace JWT.API.Controllers
     public class UserManagementController : ControllerBase
     {
         private readonly IUserManagementService _userManagementService;
+        private readonly IConfiguration _configuration;
 
-        public UserManagementController(IUserManagementService userManagementService)
+        public UserManagementController(IUserManagementService userManagementService, IConfiguration configuration)
         {
             _userManagementService = userManagementService;
+            _configuration = configuration;
         }
+
         [HttpGet()]
-        public ActionResult GetEmployees()
+        public ActionResult GetEmployeesTest()
         {
-            var employees = _userManagementService.GetEmployees();  
+            var employees = _userManagementService.GetEmployees();
             return Ok(employees);
+        }
+
+        [HttpPost("Login")]
+
+        public ActionResult Login(string user, string password)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                new Claim("UserId", "1"),
+                new Claim("DisplayName", "David"),
+                new Claim("UserName", "cruz"),
+                new Claim("Email", "cruz@abs"),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                _configuration["Jwt:Issuer"],
+                _configuration["Jwt:Audience"],
+                claims,
+                expires: DateTime.UtcNow.AddMinutes(10),
+                signingCredentials: signIn);
+
+            return Ok(new JwtSecurityTokenHandler().WriteToken(token));
         }
     }
 }
